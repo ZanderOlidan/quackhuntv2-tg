6c1a5fe4364a514a54e72c2c4a42c04ab9decf75
Zander Olidan <zander@playstudios.asia>
2020-07-03 17:07:48 +0800

    Eslint stuff

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-import Tgfancy from "tgfancy";
-import { setBot, sendMsg, hasHunt, setHasHunt, scheduleNextDuck, doAction } from "./services.js";
-import { START_HUNT, BEF, BANG } from "./textmentions.js";
-import { WEBHOOK_URL, TELEGRAM_TOKEN, WEBHOOK_PORT } from "./config.js";
-
-
+import Tgfancy from 'tgfancy';
+import { setBot, sendMsg, hasHunt, setHasHunt, scheduleNextDuck, doAction } from './services.js';
+import { START_HUNT, BEF, BANG } from './textmentions.js';
+import { WEBHOOK_URL, TELEGRAM_TOKEN, WEBHOOK_PORT } from './config.js';
+require('dotenv').config();
 
 (async () => {
-    try{
+    try {
         const t = TELEGRAM_TOKEN;
         const bot = new Tgfancy(t, {
             tgfancy: {
                 orderedSending: true,
-                ratelimiting: true,
+                ratelimiting: true
             },
             webHook: {
-                port: WEBHOOK_PORT,
-            },
+                port: WEBHOOK_PORT
+            }
         });
         setBot(bot);
 
@@ -32,16 +31,15 @@ import { WEBHOOK_URL, TELEGRAM_TOKEN, WEBHOOK_PORT } from "./config.js";
         });
 
         bot.onText(/\/bef/, async msg => {
-            await doAction(msg, BEF)
+            await doAction(msg, BEF);
         });
 
         bot.onText(/\/stophunt/, async msg => {
             if (hasHunt) {
                 setHasHunt(false);
-                await sendMsg(msg, "Hunt over. Start again with /starthunt");
+                await sendMsg(msg, 'Hunt over. Start again with /starthunt');
             }
-        })
-
+        });
     } catch (e) {
         console.log(e);
     }
