bcb9372fba773e635472be75a285d5a38a8fd45f
Zander Olidan <zander@playstudios.asia>
2020-07-03 18:06:17 +0800

    Changed docs

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 import dotenv from 'dotenv';
 dotenv.config();
 export const FAIL_RATE = 0.2;
-export const FROM_WINDOW = 10; // in minutes
-export const TO_WINDOW = 25; // in minutes
+export const FROM_WINDOW = 10 * 60; // in seconds
+export const TO_WINDOW = 25 * 60; // in secons
 export const WEBHOOK_PORT = process.env.WEBHOOK_PORT;
 export const WEBHOOK_URL = process.env.WEBHOOK_URL;
 export const SIGNED_KEY = process.env.SIGNED_KEY;
